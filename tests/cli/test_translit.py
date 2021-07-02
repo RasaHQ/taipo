64c435f05e9bd31533d91921745931df7fd24db3
Vincent <vincentwarmerdam@gmail.com>
2021-07-02 11:19:09 +0200

    small-test-refactor

--- tests/cli/test_translit.py
+++ tests/cli/test_translit.py
@@ -12,10 +12,10 @@ runner = CliRunner()
 @pytest.mark.parametrize(
     "path_in,path_out", [("nlu.yml", "nlu.yml"), ("foobar.yml", "foobar.yml")]
 )
-def test_keyboard_augment(tmp_path, path_in, path_out):
+def test_translit_augment(tmp_path, path_in, path_out):
     """Ensure basic usage of command works."""
     cmd = [
-        "keyboard",
+        "translit",
         "augment",
         "tests/data/nlu/nlu.yml",
         f"{tmp_path}/{path_in}",
@@ -26,15 +26,14 @@ def test_keyboard_augment(tmp_path, path_in, path_out):
 
 
 @pytest.mark.parametrize(
-    "lang", ["de", "en", "es", "fr", "he", "it", "nl", "pl", "th", "uk"]
+    "lang", ["ru", "mn", "sr", "bg", "ka", "uk", "el", "mk", "l1", "hy"]
 )
-def test_keyboard_lang(tmp_path, lang):
+def test_translit_lang(tmp_path, lang):
     """
     Ensure that the languages listed in nlpaug indeed work.
-    https://github.com/makcedward/nlpaug/tree/master/nlpaug/res/char/keyboard
     """
     cmd = [
-        "keyboard",
+        "translit",
         "augment",
         "tests/data/nlu/nlu.yml",
         f"{tmp_path}/nlu.yml",
@@ -46,18 +45,26 @@ def test_keyboard_lang(tmp_path, lang):
     assert nlu_path_to_dataframe(f"{tmp_path}/nlu.yml").shape == expected
 
 
-def test_keyboard_generate():
+def test_translit_generate():
     """Ensure basic usage of command works."""
     files = [
         "data/nlu-train.yml",
-        "data/typod-nlu-train.yml",
+        "data/translated-nlu-train.yml",
         "test/nlu-valid.yml",
-        "test/typod-nlu-valid.yml",
+        "test/translated-nlu-valid.yml",
     ]
     for f in files:
         if pathlib.Path(f).exists():
             pathlib.Path(f).unlink()
-    cmd = ["keyboard", "generate", "data/nlu-orig.yml", "--prefix", "typod"]
+    cmd = [
+        "translit",
+        "generate",
+        "data/nlu-orig.yml",
+        "--prefix",
+        "translated",
+        "--target",
+        "el",
+    ]
     res = runner.invoke(app, cmd)
     for f in files:
         assert pathlib.Path(f).exists()
