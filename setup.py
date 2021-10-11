3a510e432693dd0db99296f2d397d790defb2ae7
vincent d warmerdam <vincentwarmerdam@gmail.com>
2021-10-11 09:57:36 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ base_packages = [
     "typer==0.3.2",
     "nlpaug>=1.1.4",
     "parse>=1.19.0",
-    "clumper>=0.2.13",
+    "clumper>=0.2.15",
     "transliterate>=1.10.2",
     "rich>=10.4.0",
 ]
