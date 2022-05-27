fc552809112e044ac2ef3f818ba6d363c8865d0c
ka-bu <kathrin.bujna@gmail.com>
2022-05-27 15:23:12 +0200

    increase version number

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ dev_packages = [
 
 setup(
     name="taipo",
-    version="0.0.6",
+    version="0.0.7",
     packages=find_packages(exclude=["notebooks", "data"]),
     install_requires=base_packages,
     extras_require={
